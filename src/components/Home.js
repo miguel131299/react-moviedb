e794b715b620e18238c0e719ed1ba057ea025a8c
miguel <miguel131299@gmail.com>
2021-09-25 18:19:13 +0200

    Set Up API Fetching

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,49 @@
-import React, { useState } from 'react';
-//Config
+import React, { useState, useEffect } from 'react';
+// API
+import API from '../API';
+// Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-//Components
+// Components
 
-//Hook
+// Hook
 
-//Image
+// Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
 
-    return <div>Home Page</div>
-}
+        console.log("in fetch");
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState(prev => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // Initial render
+  useEffect(() => {
+      console.log("here")
+    fetchMovies(1);
+  }, []);
+
+  console.log(state);
+
+  return <div>Home Page</div>;
+};
 
 export default Home;
